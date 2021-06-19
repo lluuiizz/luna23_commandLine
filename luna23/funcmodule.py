46021fe18fef69f7f043401c4c081d7530847b65
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-19 14:23:18 -0300

    issue to fix in json file

--- luna23/funcmodule.py
+++ luna23/funcmodule.py
@@ -1,17 +1,11 @@
 import json
 # Import a json file with all characters replacements.
-with open('./json/encrypt-decrypt.json', 'r') as arq_encrypt:
-    encrypt = json.load(arq_encrypt)['encrypt']
-with open('./json/encrypt-decrypt.json', 'r') as arq_decrypt:
-    decrypt = json.load(arq_decrypt)['decrypt']
-
+f = open('data.json')
+encrypt = json.load(f[encrypt])
+decrypt = json.load(f[decrypt])
 def encrypt(text_to_encrypt):
     '''print('text from my_function :: {}'.format(text_to_encrypt))'''
-    i = 0
-    output_text = ""
-    while i < len(text_to_encrypt):
-        output_text = output_text + encrypt[text_to_encrypt[i]]
-        i += 1
+    print(encrypt)
     print('OUTPUT: ' + output_text)
 def decrypt(text_to_decrypt):
     '''print('text from my_function :: {}'.format(text_to_decrypt))'''
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name = 'luna23',
     version = '0.1.0',
-    packages = ['luna23', './luna23/json'],
+    packages = ['luna23'],
     entry_points = {
         'console_scripts' : [
             'luna23 = luna23.__main__:main'
