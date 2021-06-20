fcc958c534267b6bfe3bc38471a7352074a28097
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-20 15:48:41 -0300

    additioned relative path to package

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+import os
 setup(
     name = 'luna23',
     version = '0.1.0',
-    packages = ['luna23'],
+    packages = [os.path.dirname(os.path.abspath(__file__))+'/luna23'],
     entry_points = {
         'console_scripts' : [
             'luna23 = luna23.__main__:main'
