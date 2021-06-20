629e042bd4a4a1ee9b484e755da72f2053df0bcf
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-20 14:52:37 -0300

    issue in path of data.json solved

--- luna23/funcmodule.py
+++ luna23/funcmodule.py
@@ -1,8 +1,12 @@
 import json
+import os
+# Get the path of the directory 
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
 # Import a json file with all characters replacements.
-with open('/home/luiz/Documents/github/Python/luna23_commandLine/luna23/data.json', 'r') as arq_encrypt:
+with open(dir_path +'/data.json', 'r') as arq_encrypt:
     encrypt = json.load(arq_encrypt)['encrypt']
-with open('/home/luiz/Documents/github/Python/luna23_commandLine/luna23/data.json', 'r') as arq_decrypt:
+with open(dir_path +'/data.json', 'r') as arq_decrypt:
     decrypt = json.load(arq_decrypt)['decrypt']
 
 
@@ -13,7 +17,11 @@ def encrypt_text(text_to_encrypt):
         output_text = output_text + encrypt[text_to_encrypt[i]]
         i += 1
     print("―――――――――――――――――――――――――――――――\nOUTPUT: "+output_text)
+
+
 def decrypt_text(text_to_decrypt):
+    i = 0
+    output_text = ""
     while i < len(text_to_decrypt):
         output_text = output_text + decrypt[text_to_decrypt[i]]
         i += 1
