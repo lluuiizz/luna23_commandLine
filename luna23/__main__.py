77e2ae398c21288545f13486c18d495ed3363e34
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-20 12:50:13 -0300

    problem finale solved

--- luna23/__main__.py
+++ luna23/__main__.py
@@ -1,28 +1,21 @@
 import sys 
 from .classmodule import MyClass
-from .funcmodule import encrypt
+from .funcmodule import encrypt_text
+from .funcmodule import decrypt_text
 def main():
     args = sys.argv[1:]
     if args != []:
         if args[0] == 'encrypt' and len(args) == 1:
             text = input('INPUT: ')
-            encrypt(text)
+            encrypt_text(text)
         elif args[0] == 'decrypt' and len(args) == 1:
             text = input('INPUT: ')
+            decrypt_text(text)
         else:
             print('YOU NEED TO PASS A VALID ARGUMENT LIKE:\nencrypt\ndecrypt\n--------------------------------------')
     else: 
         print('YOU NEED TO PASS A VALID ARGUMENT LIKE:\nencrypt\ndecrypt\n--------------------------------------')
-    '''print('count of args :: {}'.format(len(args)))
-    for arg in args: 
-        print ('passed argument :: {}'.format(arg))
-    my_function('hello world')
-    if len(args) != 0:
-        my_object = MyClass('{}'.format(args[0]))
-        my_object.say_name()
-    else:
-        print('No name inputed!!!')'''
-    
+
 
 
 if __name__ == '__main__':
--- luna23/funcmodule.py
+++ luna23/funcmodule.py
@@ -1,16 +1,21 @@
 import json
 # Import a json file with all characters replacements.
-with open("data.json") as file:
-    encrypt_dic = json.load(file)["encrypt"]
-with open("data.json") as file:
-    decrypt_dic = json.load(file)["decrypt"]
-
-def encrypt(text_to_encrypt):
-    '''print('text from my_function :: {}'.format(text_to_encrypt))'''
-    print('encrypt')
-def decrypt(text_to_decrypt):
-    '''print('text from my_function :: {}'.format(text_to_decrypt))'''
-    print('decrypt')
+with open('data.json', 'r') as arq_encrypt:
+    encrypt = json.load(arq_encrypt)['encrypt']
+with open('data.json', 'r') as arq_decrypt:
+    decrypt = json.load(arq_decrypt)['decrypt']
 
 
+def encrypt_text(text_to_encrypt):
+    i = 0
+    output_text = ""
+    while i < len(text_to_encrypt):
+        output_text = output_text + encrypt[text_to_encrypt[i]]
+        i += 1
+    print("―――――――――――――――――――――――――――――――\nOUTPUT: "+output_text)
+def decrypt_text(text_to_decrypt):
+    while i < len(text_to_decrypt):
+        output_text = output_text + decrypt[text_to_decrypt[i]]
+        i += 1
+    print("―――――――――――――――――――――――――――――――\nOUTPUT: "+output_text)
 
