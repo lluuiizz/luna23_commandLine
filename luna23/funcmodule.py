add4d2fd13ae8f413c2d7e2c3d8460958eddea2a
luizguilhermegzg <uminutilqualquer3144@gmail.com>
2021-06-20 12:03:37 -0300

    building functions

--- luna23/funcmodule.py
+++ luna23/funcmodule.py
@@ -1,9 +1,9 @@
 import json
 # Import a json file with all characters replacements.
 with open("data.json") as file:
-    encrypt = json.load(file)["encrypt"]
+    encrypt_dic = json.load(file)["encrypt"]
 with open("data.json") as file:
-    decrypt = json.load(file)["decrypt"]
+    decrypt_dic = json.load(file)["decrypt"]
 
 def encrypt(text_to_encrypt):
     '''print('text from my_function :: {}'.format(text_to_encrypt))'''
